f77505130b28d29708acdc19c746b6577b1e2932
Yukti Khurana <yuktikhurana99@gmail.com>
2023-01-17 15:11:51 +0530

    mobile screen navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,32 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navToggle) {
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+} 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+} 
 
 /*==================== REMOVE MENU MOBILE ====================*/
-
+const navLink = document.querySelectorAll('.nav__link')
+function closeMenuAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // On clicking on each nav__link, show-menu class should be removed
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', closeMenuAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
